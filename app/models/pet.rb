a5d2346477d29a4fae86d2ef59e8ab5711951e06
Danielid Ramirez <danielid132@gmail.com>
2021-05-29 20:30:40 -0400

    Modifying model pet for show

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -7,19 +7,31 @@ class Pet < ApplicationRecord
   end
 
   def avg_weight
-    #TODO-implement
+    w = 0
+    suma = 0
+    pet_histories.each do |history|
+      w = history.weight
+      suma = suma + w
+    end
+    prom = (suma/pet_histories.count).ceil(2)
   end
 
   def avg_height
-    #TODO-implement
+    h = 0
+    suma = 0
+    pet_histories.each do |history|
+      h = history.heigth
+      suma = suma + h
+    end
+    prom = (suma/pet_histories.count).ceil(2)
   end
 
   def max_weight
-    #TODO-implement
+    (pet_histories.map { |history| history.weight}).max
   end
 
   def max_height
-    #TODO-implement
+    (pet_histories.map { |history| history.heigth}).max
   end
 
 end
