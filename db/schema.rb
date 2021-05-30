01a2ab25d39ededb0d6982691bb026a0bbcb3506
Danielid Ramirez <danielid132@gmail.com>
2021-05-30 00:34:15 -0400

    Creating client CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_005634) do
+ActiveRecord::Schema.define(version: 2021_05_30_042409) do
 
   create_table "clients", force: :cascade do |t|
-    t.string "client"
+    t.string "name"
     t.integer "phone_number"
     t.string "email"
     t.datetime "created_at", null: false
